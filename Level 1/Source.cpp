f6eab41085e00f424ec1f3643877dd2ce6257f58
emmanuelwestra <ewestra@ualr.edu>
2023-09-11 16:09:25 -0500

    Function creation
    Set up functions below; working on boardSetup().

--- Level 1/Source.cpp
+++ Level 1/Source.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <random>
 #include <vector>
+#include <fstream>
 
 /*Game Constants*/
 const int gameBoardSize{ 10 };
@@ -12,20 +13,18 @@ void displayGameState(const std::vector<gamePieces>& gameBoard, bool revealMines
 void displayGameDone(const std::vector<gamePieces>& gameBoard);
 
 /* Engine Functions*/
-std::vector<gamePieces> boardSetup();
+std::vector<gamePieces>& boardSetup(int gameBoardSize);
 void changeGameState(std::vector<gamePieces>& gameBoard);
 bool isGameDone(const std::vector<gamePieces>& gameBoard);
 
 int countMines(int row, int column, const std::vector<gamePieces>& gameBoard);
 int boardIndex(int row, int column);
 
-int main()
-{
-	
+int main() {
 	char playAgain{ 'y' };
 	while (playAgain == 'y' || playAgain == 'Y')
 	{
-		std::vector<gamePieces>& gameBoard{ boardSetup() };
+		std::vector<gamePieces>& gameBoard{ boardSetup(gameBoardSize) };
 		while (!isGameDone(gameBoard))
 		{
 			displayGameState(gameBoard);
@@ -37,8 +36,9 @@ int main()
 		
 	}
 }
-void splashScreen()
-{
+
+/*GUI Functions*/
+void splashScreen() {
 	std::cout << "Mine Sweeper!" << std::endl;
 	std::cout << std::endl;
 	std::cout << "Your name here (2019)" << std::endl;
@@ -49,4 +49,16 @@ void splashScreen()
 	std::cout << std::endl;
 	std::cout << "Clear the minefield without hitting a mine!" << std::endl;
 	system("PAUSE");
+}
+void displayGameState(const std::vector<gamePieces>& gameBoard, bool revealMines = false);
+void displayGameDone(const std::vector<gamePieces>& gameBoard);
+
+/* Engine Functions*/
+std::vector<gamePieces>& boardSetup(int gameBoardSize) {
+	static std::vector<gamePieces> gamePieces(gameBoardSize);
+	return gamePieces;
+}
+void changeGameState(std::vector<gamePieces>& gameBoard);
+bool isGameDone(const std::vector<gamePieces>& gameBoard) {
+	return false; // test
 }
